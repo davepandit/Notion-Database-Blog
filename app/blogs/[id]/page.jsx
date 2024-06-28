db0326f8840fb3a6691c54d512b3e23b9bc53608
Debajyoti Pandit <codeninja0812@gmail.com>
2024-06-29 02:02:55 +0530

    Add basic functionality

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -3,7 +3,10 @@ import { getAllBlogs } from '@/lib/notion'
 
 
 
-const SingleBlog = async({params}) => {
+const SingleBlog = async({params , searchParams}) => {
+    // console.log('Here is the serachParams:', searchParams)
+    const author = searchParams.author
+    const createdAt = searchParams.createdAt
     // doubt why cant I use the function in order to get the id 
 
     //find the blog from all the blogs which have the id as that of the id form the params
@@ -18,6 +21,12 @@ const SingleBlog = async({params}) => {
         </div>
         <div>
             {blog.content}
+        </div>
+        <div>
+            Author is:{author}
+        </div>
+        <div>
+            Created At:{createdAt}
         </div>
     </>
   )
--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -4,13 +4,14 @@ import { getAllBlogs } from '@/lib/notion'
 const AllBlogs = async() => {
     //get the data here
     const blogs = await getAllBlogs()
+    // console.log('Here goes all the blogs:', JSON.stringify(blogs))
   return (
     <>
         <div>AllBlogs</div>
         <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
-            <a href={`/blogs/${blog.id}`}>{blog.title}</a>
+            <a href={`/blogs/${blog.id}?author=${blog.author}&createdAt=${blog.createdAt}`}>{blog.title}</a>
           </li>
         ))}
       </ul>
